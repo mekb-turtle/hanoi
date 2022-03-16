db624caa655876329d278c1f661ba0b2384d9eec
mekb the turtle <mekbgithub@icloud.com>
2022-03-16 19:33:19 +1100

    hmm

--- index.js
+++ index.js
@@ -16,21 +16,23 @@ const move = (i, j) => {
 };
 const printTower = () => {
 	let len = Math.max(...tower.map(e => Math.max(...e)));
-	s = new Array(len+3).fill("");
-	for (let j = 0; j < len+3; ++j) {
-		let ring = (len+2)-j;
+	s = new Array(len+1).fill("");
+	for (let j = 0; j < s.length; ++j) {
+		let ring = s.length-1-j;
 		for (let i = 0; i < tower.length; ++i) {
 			let val = tower[i][ring]*2||0;
 			let str =
-				" ".repeat(len-val/2+2) +
+				" ".repeat(len-val/2+2-(val?0:1)) +
 				"─".repeat(val&&Math.floor(val/2-1)) +
-				["┼┴"[+!ring],""][+!val];
+				["┼┴"[+!ring],"│"][+!val];
 			s[j] += str + str.split("").reverse().splice(1).join("");
 		}
 	}
+	console.log();
 	for (let y = 0; y < s.length; ++y) {
 		console.log(s[y]);
 	}
+	console.log();
 }
 printTower();
 })();
